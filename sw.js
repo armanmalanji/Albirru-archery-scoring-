846884f9fdab134d008dc0cfa648c9c2e6f12a1e
armanmalanji <113245184+armanmalanji@users.noreply.github.com>
2026-07-20 18:49:36 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,46 +1,45 @@
-const CACHE_NAME = 'albirru-scoring-v1';
+const CACHE_NAME = 'albirru-scoring-v4';
 const urlsToCache = [
-'./',
-'./index.html',
-'./manifest.json',
-'./icon-192.png',
-'./icon-512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png'
 ];
 
 // Install Service Worker dan simpan file ke cache
 self.addEventListener('install', event => {
-event.waitUntil(
-caches.open(CACHE_NAME)
-.then(cache => {
-console.log('Cache berhasil dibuka');
-return cache.addAll(urlsToCache);
-})
-);
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
-// Ambil data dari cache saat offline
+// Ambil file dari cache saat offline
 self.addEventListener('fetch', event => {
-event.respondWith(
-caches.match(event.request)
-.then(response => {
-// Jika ada di cache, tampilkan. Jika tidak, ambil dari internet
-return response || fetch(event.request);
-})
-);
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Jika ada di cache, gunakan itu. Jika tidak, ambil dari internet.
+        return response || fetch(event.request);
+      })
+  );
 });
 
-// Update Service Worker dan hapus cache versi lama jika ada pembaruan kode
+// Hapus cache lama jika ada pembaruan versi
 self.addEventListener('activate', event => {
-const cacheWhitelist = [CACHE_NAME];
-event.waitUntil(
-caches.keys().then(cacheNames => {
-return Promise.all(
-cacheNames.map(cacheName => {
-if (cacheWhitelist.indexOf(cacheName) === -1) {
-return caches.delete(cacheName);
-}
-})
-);
-})
-);
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
